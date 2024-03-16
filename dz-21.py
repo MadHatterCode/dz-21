fc6d1efcc2b84fd9ce92216ec79b64654dcbe2a6
Sergii <sadante@gmail.com>
2024-03-16 14:50:23 +0200

    dz-21 solution

---
+++ dz-21.py
@@ -0,0 +1,12 @@
+def popular_words(text, words):
+    l_text = text.lower()
+    word_counts = {}.fromkeys(words, 0)
+    for word in l_text.split():
+        if word in word_counts:
+            word_counts[word] += 1
+    return word_counts
+
+
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''',
+                     ['i', 'was', 'three', 'near']) == {'i': 4, 'was': 3, 'three': 0, 'near': 0}, 'Test1'
+print('OK')
